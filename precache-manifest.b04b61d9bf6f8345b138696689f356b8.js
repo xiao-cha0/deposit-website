c9d3361844ea570db1fe879b931a3747373ed3d2
justin <1150216807@qq.com>
2020-03-10 13:48:07 +0800

    deploy

---
+++ precache-manifest.b04b61d9bf6f8345b138696689f356b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec218e4da9a0f3d6f0c4",
+    "url": "/deposit-website/css/app.04b22ad9.css"
+  },
+  {
+    "revision": "c6a918f7f07e7a5aad151feaeccd1b85",
+    "url": "/deposit-website/index.html"
+  },
+  {
+    "revision": "ec218e4da9a0f3d6f0c4",
+    "url": "/deposit-website/js/app.eb46c2c2.js"
+  },
+  {
+    "revision": "55a6c96e8ec24bffd4f2",
+    "url": "/deposit-website/js/chunk-vendors.6f4e76d2.js"
+  },
+  {
+    "revision": "3e2de1923d6a75ae01e9f54a588c0fd5",
+    "url": "/deposit-website/manifest.json"
+  },
+  {
+    "revision": "7893be2de9d60539a8d1735b37e88fb7",
+    "url": "/deposit-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deposit-website/robots.txt"
+  }
+]);
